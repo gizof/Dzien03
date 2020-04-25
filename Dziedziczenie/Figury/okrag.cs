7b34b0ab55e746e3c650e944d03aa84d34a07768
gizof <64309563+gizof@users.noreply.github.com>
2020-04-25 14:26:27 +0200

    Update okrag.cs

--- Dziedziczenie/Figury/okrag.cs
+++ Dziedziczenie/Figury/okrag.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//zmiana z poziomu githuba
 namespace Dziedziczenie.Figury
 {
     class okrag
     {
         private double srednica;
+        
+        public double ObliczPole()
+        {
+            return 3.14159 * (0.5 *srednica) * (0.5 *srednica);
+        }
     }
 }
